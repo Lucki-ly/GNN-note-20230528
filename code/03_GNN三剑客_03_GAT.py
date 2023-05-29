c92c15bd9d2bd75a0aa036c465c1f5c4535a3d99
Lucki-ly <62681225+Lucki-ly@users.noreply.github.com>
2023-05-29 15:00:44 +0800

    ok

---
+++ code/03_GNN三剑客_03_GAT.py
@@ -0,0 +1,265 @@
+#建议用google colab跑！！！速度巨快（除了GAT，这是真慢）
+import scipy.sparse as sp
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import os.path as osp
+import pickle
+import numpy as np
+import itertools
+import collections
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# 保存处理好的数据
+Data = collections.namedtuple('Data', ['x', 'y', 'adjacency_dict',
+                           'train_mask', 'val_mask', 'test_mask'])
+
+# 处理Cora数据
+class CoraData(object):
+
+    filenames = ["ind.cora.{}".format(name) for name in ['x', 'tx', 'allx', 'y', 'ty', 'ally', 'graph', 'test.index']]
+
+    def __init__(self, data_root="D:\\GNN\\GNN_Notes-master\\code\\data\\cora", rebuild=False):
+        """包括数据下载、处理、加载等功能 当数据的缓存文件存在时，将使用缓存文件，否则将下载、处理，并缓存到磁盘
+        Args:-------
+        data_root: string, optional 存放数据的目录，原始数据路径: {data_root}/raw 缓存数据路径: {data_root}/processed_cora.pkl
+        rebuild: boolean, optional 是否需要重新构建数据集，当设为True时，如果缓存数据存在也会重建数据"""
+        self.data_root = data_root
+        save_file = osp.join(self.data_root, "processed_cora.pkl")
+        if osp.exists(save_file) and not rebuild:
+            print("Using Cached file: {}".format(save_file))
+            self._data = pickle.load(open(save_file, "rb"))
+        else:
+            # self.maybe_download()
+            self._data = self.process_data()
+            with open(save_file, "wb") as f:
+                pickle.dump(self.data, f)
+        print("Cached file: {}".format(save_file))
+
+    @property
+    def data(self):
+        """返回Data数据对象，包括x, y, adjacency, train_mask, val_mask, test_mask"""
+        return self._data
+
+    def process_data(self):
+        """ 处理数据，得到节点特征和标签，邻接矩阵，训练集、验证集以及测试集 """
+        print("Process data ...")
+        _, tx, allx, y, ty, ally, graph, test_index = [self.read_data(osp.join(self.data_root, name)) for name in
+                                                       self.filenames]
+        train_index = np.arange(y.shape[0])
+        val_index = np.arange(y.shape[0], y.shape[0] + 500)
+        sorted_test_index = sorted(test_index)
+        x = np.concatenate((allx, tx), axis=0)
+        y = np.concatenate((ally, ty), axis=0).argmax(axis=1)
+        x[test_index] = x[sorted_test_index]
+        y[test_index] = y[sorted_test_index]
+        num_nodes = x.shape[0]
+        train_mask = np.zeros(num_nodes, dtype=bool)
+        val_mask = np.zeros(num_nodes, dtype=bool)
+        test_mask = np.zeros(num_nodes, dtype=bool)
+        train_mask[train_index] = True
+        val_mask[val_index] = True
+        test_mask[test_index] = True
+        adjacency_dict = graph
+        print("Node's feature shape: ", x.shape)
+        print("Node's label shape: ", y.shape)
+        print("Adjacency's shape: ", len(adjacency_dict))
+        print("Number of training nodes: ", train_mask.sum())
+        print("Number of validation nodes: ", val_mask.sum())
+        print("Number of test nodes: ", test_mask.sum())
+
+        return Data(x=x, y=y, adjacency_dict=adjacency_dict,
+                    train_mask=train_mask, val_mask=val_mask, test_mask=test_mask)
+
+    @staticmethod
+    def build_adjacency(adj_dict):
+        """根据邻接表创建邻接矩阵"""
+        edge_index = []
+        num_nodes = len(adj_dict)
+        for src, dst in adj_dict.items():
+            edge_index.extend([src, v] for v in dst)
+            edge_index.extend([v, src] for v in dst)
+        # 由于上述得到的结果中存在重复的边，删掉这些重复的边
+        edge_index = list(k for k, _ in itertools.groupby(sorted(edge_index)))
+        edge_index = np.asarray(edge_index)
+        adjacency = sp.coo_matrix((np.ones(len(edge_index)), (edge_index[:, 0], edge_index[:, 1])),
+                                  shape=(num_nodes, num_nodes), dtype="float32")
+        return adjacency
+
+    @staticmethod
+    def read_data(path):
+        """使用不同的方式读取原始数据以进一步处理"""
+        name = osp.basename(path)
+        if name == "ind.cora.test.index":
+            out = np.genfromtxt(path, dtype="int64")
+            return out
+        else:
+            out = pickle.load(open(path, "rb"), encoding="latin1")
+        out = out.toarray() if hasattr(out, "toarray") else out
+        return out
+
+
+class GraphAttentionLayer(nn.Module):
+    """
+    Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
+    图注意力层
+    """
+
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True):
+        super(GraphAttentionLayer, self).__init__()
+        self.in_features = in_features  # 节点表示向量的输入特征维度
+        self.out_features = out_features  # 节点表示向量的输出特征维度
+        self.dropout = dropout  # dropout参数
+        self.alpha = alpha  # leakyrelu激活的参数
+        self.concat = concat  # 如果为true, 再进行elu激活
+
+        # 定义可训练参数，即论文中的W和a
+        self.W = nn.Parameter(torch.zeros(size=(in_features, out_features)))
+        nn.init.xavier_uniform_(self.W.data, gain=1.414)  # xavier初始化
+        self.a = nn.Parameter(torch.zeros(size=(2 * out_features, 1)))
+        nn.init.xavier_uniform_(self.a.data, gain=1.414)  # xavier初始化
+
+        # 定义leakyrelu激活函数
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
+
+    def forward(self, inp, adj):
+        """
+        inp: input_fea [N, in_features]  in_features表示节点的输入特征向量元素个数
+        adj: 图的邻接矩阵  [N, N] 非零即一，数据结构基本知识
+        """
+        h = torch.mm(inp, self.W)  # [N, out_features]
+        N = h.size()[0]  # N 图的节点数
+
+        a_input = torch.cat([h.repeat(1, N).view(N * N, -1), h.repeat(N, 1)], dim=1).view(N, -1, 2 * self.out_features)
+        # [N, N, 2*out_features]
+        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))
+        # [N, N, 1] => [N, N] 图注意力的相关系数（未归一化）
+
+        zero_vec = -1e12 * torch.ones_like(e)  # 将没有连接的边置为负无穷
+        attention = torch.where(adj.to_dense() > 0, e, zero_vec)  # [N, N]
+        # 表示如果邻接矩阵元素大于0时，则两个节点有连接，该位置的注意力系数保留，
+        # 否则需要mask并置为非常小的值，原因是softmax的时候这个最小值会不考虑。
+        attention = F.softmax(attention, dim=1)  # softmax形状保持不变 [N, N]，得到归一化的注意力权重！
+        attention = F.dropout(attention, self.dropout, training=self.training)  # dropout，防止过拟合
+        h_prime = torch.matmul(attention, h)  # [N, N].[N, out_features] => [N, out_features]
+        # 得到由周围节点通过注意力权重进行更新的表示
+        if self.concat:
+            return F.elu(h_prime)
+        else:
+            return h_prime
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
+
+
+class GAT(nn.Module):
+    def __init__(self, n_feat, n_hid, n_class, dropout, alpha, n_heads):
+        """Dense version of GAT
+        n_heads 表示有几个GAL层，最后进行拼接在一起，类似self-attention
+        从不同的子空间进行抽取特征。
+        """
+        super(GAT, self).__init__()
+        self.dropout = dropout
+
+        # 定义multi-head的图注意力层
+        self.attentions = [GraphAttentionLayer(n_feat, n_hid, dropout=dropout, alpha=alpha, concat=True) for _ in
+                           range(n_heads)]
+        for i, attention in enumerate(self.attentions):
+            self.add_module('attention_{}'.format(i), attention)  # 加入pytorch的Module模块
+        # 输出层，也通过图注意力层来实现，可实现分类、预测等功能
+        self.out_att = GraphAttentionLayer(n_hid * n_heads, n_class, dropout=dropout, alpha=alpha, concat=False)
+
+    def forward(self, x, adj):
+        x = F.dropout(x, self.dropout, training=self.training)  # dropout，防止过拟合
+        x = torch.cat([att(x, adj) for att in self.attentions], dim=1)  # 将每个head得到的表示进行拼接
+        x = F.dropout(x, self.dropout, training=self.training)  # dropout，防止过拟合
+        x = F.elu(self.out_att(x, adj))  # 输出并激活
+        return F.log_softmax(x, dim=1)  # log_softmax速度变快，保持数值稳定
+
+
+def accuracy(mask):
+    model.eval()
+    with torch.no_grad():
+        logits = model(tensor_x,tensor_adjacency)
+        test_mask_logits = logits[mask]
+        predict_y = test_mask_logits.max(1)[1]
+        accuracy = torch.eq(predict_y, tensor_y[mask]).float().mean()
+    return accuracy
+
+
+if __name__ == "__main__":
+    #1.加载数据
+    dataset = CoraData().data
+    #如果有GPU则使用GPU
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    #接着预处理剩下的数据
+    x = dataset.x / dataset.x.sum(1, keepdims=True)
+    # 归一化数据，使得每一行和为1
+    tensor_x = torch.from_numpy(x).to(device)
+    tensor_y = torch.from_numpy(dataset.y).to(device)
+    tensor_train_mask = torch.from_numpy(dataset.train_mask).to(device)
+    tensor_val_mask = torch.from_numpy(dataset.val_mask).to(device)
+    tensor_test_mask = torch.from_numpy(dataset.test_mask).to(device)
+    normalize_adjacency = dataset.adjacency_dict
+    # 规范化邻接矩阵
+    indices = torch.from_numpy(np.asarray([normalize_adjacency.row, normalize_adjacency.col])).long()
+    values = torch.from_numpy(normalize_adjacency.data.astype(np.float32))
+    tensor_adjacency = torch.sparse.FloatTensor(indices, values, (2708, 2708)).to(device)
+    #2.构建模型
+    model = GAT(1433, 16, 7, 0.2, 0.2, 4).to(device)
+    #3.训练模型
+    learning_rate = 0.1
+    weight_decay = 5e-4
+    epochs = 5
+    # 损失函数使用交叉熵
+    criterion = nn.CrossEntropyLoss().to(device)
+    # 优化器使用Adam
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    # 训练
+    loss_history = []
+    val_acc_history = []
+    model.train()
+    train_y = tensor_y[tensor_train_mask]
+    for epoch in range(epochs):
+        # 前向传播
+        logits = model(tensor_x,tensor_adjacency)
+        # 只选择训练节点进行监督
+        train_mask_logits = logits[tensor_train_mask]
+        # 计算损失值
+        loss = criterion(train_mask_logits, train_y)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        # 反向传播计算参数的梯度
+        optimizer.step()
+        # 使用优化方法进行梯度更新
+        train_acc = accuracy(tensor_train_mask)
+        # 计算当前模型在训练集上的准确率
+        val_acc = accuracy(tensor_val_mask)
+        # 计算当前模型在验证集上的准确率
+        # 记录训练过程中损失值和准确率的变化，用于画图
+        loss_history.append(loss.item())
+        val_acc_history.append(val_acc.item())
+        print("Epoch {:03d}: Loss {:.4f}, TrainAcc {:.4}, ValAcc {:.4f}".format(epoch, loss.item(), train_acc.item(), val_acc.item()))
+    #4.查看效果
+    import warnings
+    warnings.filterwarnings("ignore",category=DeprecationWarning)
+    #%matplotlib inline
+    #对X用tsne降维
+    from sklearn import manifold
+    tsne = manifold.TSNE(n_components=2,init="pca")
+    X_tsne = tsne.fit_transform(tensor_x.numpy())
+    #预测
+    pred = model(tensor_x,tensor_adjacency).max(1)[1].numpy()
+    #归一化显示
+    x_min, x_max = X_tsne.min(0), X_tsne.max(0)
+    X_norm = (X_tsne - x_min) / (x_max - x_min)
+    plt.figure(figsize=(12, 9))
+    plt.scatter(X_norm[:,0],X_norm[:,1],c=pred)
+    plt.yticks([])
+    plt.xticks([])
+    plt.show()
